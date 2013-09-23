71f0a809d632741ed1b117779722347f5c11ef74
rahul <rahul2501350@gmail.com>
2013-09-23 13:40:01 +0530

    launch page with team and contact form

--- app/assets/javascripts/launch.js
+++ app/assets/javascripts/launch.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	$("#team").on("click", function(){
 		$(".launch-team").toggle();
+		$(".launch-contact").hide();
 	});
+
+
+	$("#launch-contact-btn").on("click", function(){
+		$(".launch-contact").toggle();
+		$(".launch-team").hide();
+
+	});
+
 });
